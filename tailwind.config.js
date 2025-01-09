1b23ac8d47e1eefc36918cafcb984a958df860fa
Daiki Coder <daiki.coder@gmail.com>
2025-01-09 13:20:13 -0300

    Added prettier Added tailwind screens Added tailwind spacing Added Google Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      inherit: 'inherit',
+      current: 'currentColor',
+      white: '#ffffff',
+    },
+    screens: {
+      sm: '360px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
+    spacing: {
+      none: '0px',
+      xs: '4px',
+      sm: '8px',
+      md: '12px',
+      lg: '16px',
+      xl: '24px',
+      '2xl': '32px',
+      '3xl': '40px',
+      '4xl': '48px',
+      '5xl': '56px',
+      '6xl': '64px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
