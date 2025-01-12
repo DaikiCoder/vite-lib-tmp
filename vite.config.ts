ccbce29a6d1b287aaea6400c35051bfdc78aafdb
Daiki Coder <daiki.coder@gmail.com>
2025-01-12 11:51:14 -0300

    Added lib inject css

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: ['src/lib/main.ts'],
       name: 'DaikiUI',
       formats: ['es'],
+      /* entry: ['src/lib/main.ts'], */
+      entry: {
+        index: 'src/lib/main.ts',
+        'components/button/button': 'src/lib/components/button/',
+        'components/card/card': 'src/lib/components/card/',
+      },
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
+      output: {
+        // Put chunk files at <output>/chunks
+        chunkFileNames: 'chunks/[name].[hash].js',
+        // Put chunk styles at <output>/assets
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
     },
   },
 });
